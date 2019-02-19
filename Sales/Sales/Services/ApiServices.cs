eded47bb28124be1acf0b3573ba2ededc680a6c1
araigeorge <araigeorge@gmail.com>
2019-02-18 20:56:38 -0400

    nose

---
+++ Sales/Sales/Services/ApiServices.cs
@@ -0,0 +1,48 @@
+﻿namespace Sales.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using Sales.Common.Models;
+
+    public class ApiServices
+    {
+        public async Task<Response> GetList<T>(string urlBase, string prefix, string controller)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var url = $"{prefix}{controller}";
+                var response = await client.GetAsync(url);
+                var answer = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        Issuccess = false,
+                        Message = answer,
+                    };
+                }
+                var list = JsonConvert.DeserializeObject<List<T>>(answer);
+                return new Response
+                {
+                    Issuccess = true,
+                    Result = list,
+                };
+            }
+            catch (Exception Ex)
+            {
+                return new Response
+                {
+                    Issuccess  = false,
+                    Message = Ex.Message,
+                };
+            }
+
+        }
+
+    }
+}
